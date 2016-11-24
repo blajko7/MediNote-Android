9d319b1932cb51cc8236e653c54c365d6af6b448
Blajko7 <skullybunnystudios@gmail.com>
2016-11-24 21:32:09 +0200

    init commit

--- app/src/main/java/com/example/blajko7/medinote/Create_fragment.java
+++ app/src/main/java/com/example/blajko7/medinote/Create_fragment.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class Registration_fragment extends Fragment {
+public class Create_fragment extends Fragment{
 
     private void ChangeFragment(Fragment fragment, boolean addReverseTransaction)
     {
@@ -23,17 +23,8 @@ public class Registration_fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.registration_fragment, container, false);
+        View view = inflater.inflate(R.layout.create_fragment, container, false);
 
-        Button registerButton = (Button) view.findViewById(R.id.buttonRegister);
-        registerButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        ChangeFragment(new Main_fragment(),true);
-                    }
-                }
-        );
 
         return view;
     }
--- app/src/main/java/com/example/blajko7/medinote/Main_fragment.java
+++ app/src/main/java/com/example/blajko7/medinote/Main_fragment.java
@@ -2,36 +2,64 @@ package com.example.blajko7.medinote;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentTransaction;
+import android.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Main_fragment extends Fragment {
 
-
+    private void ChangeFragment(Fragment fragment, boolean addReverseTransaction)
+    {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container_home, fragment);
+        if (addReverseTransaction)
+        {
+            fragmentTransaction.addToBackStack(null);
+        }
+        fragmentTransaction.commit();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        Button createButton = (Button) view.findViewById(R.id.home_create_button);
+        createButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ChangeFragment(new Login_fragment(),true);
+                    }
+                }
+        );
+        Button checkButton = (Button) view.findViewById(R.id.home_check_button);
+        checkButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View view) {
+                        ChangeFragment(new Check_fragment(),true);
+                    }
+                }
+        );
+        Button registrationButton = (Button) view.findViewById(R.id.home_registration_button);
+        registrationButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View view) {
+                        ChangeFragment(new Registration_fragment(),true);
+                    }
+                }
+        );
+
         return view;
-    }
-
-    public void create_onClick(View view)
-    {
-        fragmentTransaction.replace(R.id.fragment, new Login_fragment());
-        fragmentTransaction.commit();
-    }
-
-    public void check_onClick(View view)
-    {
-
-    }
-    public void registration_onClick(View view)
-    {
-
     }
 }
